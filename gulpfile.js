0014954b1da4ae96e456f51bc3906412be81385e
cschmitz <schmitz.chris@gmail.com>
2016-03-05 14:09:38 -0600

    Updates gulp file to "better" handle errors.
    At the very least it doesn’t kill gulp when an error happens :P

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var connect = require('gulp-connect');
 var gutil = require('gutil');
+var notify = require('gulp-notify');
 
 gulp.task('default', ['watch', 'serve']);
 
@@ -14,10 +15,28 @@ gulp.task('serve', function (){
     });
 });
 
+gulp.task('testerror', function (){
+    console.log('fired');
+    notify('test');
+
+});
+
+function handleErrors(error){
+    console.log(error);
+    notify('test');
+    // notify.onError({
+    //     title: 'Compilation error',
+    //     message: 'There was an error.'
+    // });
+    this.emit('end');
+}
+
 gulp.task('build-app', function (){
     return browserify('src/app/app.js')
-        .transform(vueify).on('error', gutil.log)
+        .transform(vueify)
+        .on('error', handleErrors)
         .bundle()
+        .on('error', handleErrors)
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('public'));
 });
